fc1386986d4610f257925d699a375975d0b0a057
cludwell <cludwell@gmail.com>
2023-04-18 13:41:10 -0700

    albums table

---
+++ app/models/albums.py
@@ -0,0 +1,29 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.sql import func
+
+class Album(db.Model):
+    __tablename__ = 'albums'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Integer)
+    band_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+    album_image = db.Column(db.String(255), nullable=False)
+    genre = db.Column(db.String(255), nullable=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'bandId': self.band_id,
+            'albumImage': self.album_image,
+            'genre': self.genre,
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at
+        }
