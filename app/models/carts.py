ad6635cd1e3b853581fd8eb3564f2ed4c523b87c
cludwell <cludwell@gmail.com>
2023-04-18 12:54:08 -0700

    cart table

---
+++ app/models/carts.py
@@ -0,0 +1,25 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.sql import func
+
+
+class Cart(db.Model):
+    __tablename__ = 'carts'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('user.id')))
+    item_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('albums.id')))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.user_id,
+            'itemId': self.item_id,
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at
+        }
+    
