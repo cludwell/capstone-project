18d6577792d29dbf232350eb07639045babb223c
cludwell <cludwell@gmail.com>
2023-04-21 16:12:19 -0700

    upcoming livestream

--- react-app/src/components/Landing/index.js
+++ react-app/src/components/Landing/index.js
@@ -3,7 +3,8 @@ import './Landing.css'
 import { useEffect } from 'react'
 import { fetchAlbums } from '../../store/albums'
 import NewAndNotable from '../NewAndNotable'
-import { fetchAllPurchases, loadAllPurchases } from '../../store/purchases'
+import { fetchAllPurchases } from '../../store/purchases'
+import UpcomingLiveStream from '../UpcomingLiveStream'
 
 export default function Landing() {
     const dispatch = useDispatch()
@@ -16,7 +17,13 @@ export default function Landing() {
     const albums = useSelector(state => state.albums.allAlbums)
     const state = useSelector(state => state)
     console.log('STATE', state)
-
+    const livestreamTimes = [
+        ['in two days', '7:00 PM PST'],
+        ['tomorrow', '2:00 PM PST'],
+        ['tomorrow', '8:00 PM PST'],
+        ['today', '10:00 PM PST'],
+        ['today', '6:00 PM PST'],
+    ]
     if (!albums || !Object.values(albums).length) return null
 
     return (
@@ -24,7 +31,12 @@ export default function Landing() {
 
             <h3 className='fans-have-paid'>Fans have paid artists billions using fancamp, and $190 in the last year</h3>
 
-            
+            <h5 className='new-notable-title'>UPCOMING FANCAMP LIVE STREAMS</h5>
+            <div className='upcoming-live-streams-container'>
+            {Object.values(albums).map((a,i) => (
+                <UpcomingLiveStream ele={livestreamTimes[i]} album={a} />
+            )).reverse().slice(5, 10)}
+            </div>
             <h5 className='new-notable-title'>NEW AND NOTABLE</h5>
             <div className='new-and-notable-container'>
             {Object.values(albums).map((album,i)=> (
