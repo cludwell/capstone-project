ed356cffea44378582b4173bfecf5447b05aac27
cludwell <cludwell@gmail.com>
2023-04-20 22:16:22 -0700

    albums thunk

--- react-app/src/components/Landing/index.js
+++ react-app/src/components/Landing/index.js
@@ -1,9 +1,16 @@
+import { useDispatch } from 'react-redux'
 import './Landing.css'
+import { useEffect } from 'react'
 
 export default function Landing() {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch()
+    }, [dispatch])
     return (
         <div className='landing-container'>
-            
+
         </div>
     )
 }
--- react-app/src/store/albums.js
+++ react-app/src/store/albums.js
@@ -1,10 +1,19 @@
 const LOAD_ALBUMS = 'albums/LOAD_ALBUMS'
 
 //load all shops
-export const loadShopsAction = albums => {
+export const loadAlbums = albums => {
     return {
         type: LOAD_ALBUMS,
         albums
+    }
+}
+
+//thunk for loading all albums
+export const fetchAlbums = () => async dispatch => {
+    const response = await fetch('/api/albums/')
+    if (response.ok) {
+        const albums = await response.json()
+        dispatch(loadAlbums(albums))
     }
 }
 
