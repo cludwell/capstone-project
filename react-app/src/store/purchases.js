03c0686632ce7252d4f9b9d7a81e036e42060e89
cludwell <cludwell@gmail.com>
2023-04-21 08:48:20 -0700

    thunk all purchases

--- react-app/src/store/purchases.js
+++ react-app/src/store/purchases.js
@@ -1,29 +1,43 @@
 import { response } from "express"
 
-const LOAD_PURCHASES = 'purchases/LOAD_PURCHASES'
-
+const LOAD_USER_PURCHASES = 'purchases/LOAD_USER_PURCHASES'
+const LOAD_ALL_PURCHASES = 'purchases/LOAD_ALL_PURCHASES'
 //load all user buys
-export const loadPurchases = purchases => {
+export const loadUserPurchases = purchases => {
     return {
-        type: LOAD_PURCHASES,
+        type: LOAD_USER_PURCHASES,
         purchases
     }
 }
-
+export const loadAllPurchases = purchases => {
+    return {
+        type: LOAD_ALL_PURCHASES,
+        purchases
+    }
+}
 //thunk for loading purchases
 export const fetchUserPurchases = () => async dispatch => {
     const response = await fetch('/api/purchases/user')
     if (response.ok) {
         const purchases = await response.json()
-        dispatch(loadPurchases(purchases))
+        dispatch(loadUserPurchases(purchases))
         return purchases
+    }
+}
+export const fetchAllPurchases = () => async dispatch => {
+    const response = await fetch('/api/purchases/')
+    if (response.ok) {
+        const purchases = await response.json()
+        dispatch(load)
     }
 }
 const initialState = {}
 export default function purchasesReducer (state = initialState, action) {
     switch(action.type) {
-        case LOAD_PURCHASES:
-            return {...state, user: {...action.purchases}}
+        case LOAD_USER_PURCHASES:
+            return { ...state, user: { ...action.purchases } }
+        case LOAD_ALL_PURCHASES:
+            return { ...state, allPurchases: { ...action.purchases} }
         default: return state
     }
 }
