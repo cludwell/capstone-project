06ce20172af25b5cf8476fd323a0adfa93d700bb
cludwell <cludwell@gmail.com>
2023-04-22 20:50:03 -0700

    post band route written

--- app/api/band_routes.py
+++ app/api/band_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, redirect
 from app.models import Album, Song, Purchase, User, Band, db
 from flask_login import current_user, login_required
-
+from app.forms import PostBandForm
 band_routes = Blueprint('/bands', __name__)
 
 def get_albums_by_band(band_id):
@@ -27,3 +27,31 @@ def bands_albums(band_id):
             db.session.delete(band)
             db.session.commit()
             return band.to_dict()
+
+@band_routes('/', methods=['POST'])
+def post_band():
+    """post a band"""
+    if request.method == 'POST' and current_user.id:
+        form = PostBandForm()
+        form['csrf_token'].data = request.cookies['csrf_token']
+        form.validate_on_submit()
+        if not form.validate_on_submit():
+            return form.errors, 404
+        if form.validate_on_submit():
+            new_band = Band(
+                name = form.date['name'],
+                city = form.date['city'],
+                state = form.date['state'],
+                country = form.date['country'],
+                artist_image = form.date['artist_image'],
+                banner_url = form.date['banner_url'],
+                description = form.date['description'],
+                genres = form.date['genres'],
+                user_id = current_user.id,
+                name = form.date['name'],
+            )
+            db.session.add(new_band)
+            db.session.commit()
+            return new_band.to_dict(), 201
+    else:
+        return {"errors": "You must be validated"}
