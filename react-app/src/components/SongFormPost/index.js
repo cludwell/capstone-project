23ad2b2af782fee4837006ddceef03bfdac3b4d0
cludwell <cludwell@gmail.com>
2023-04-27 18:17:37 -0700

    post song vals

--- react-app/src/components/SongFormPost/index.js
+++ react-app/src/components/SongFormPost/index.js
@@ -1,6 +1,6 @@
 // import { useParams } from 'react-router-dom'
 import './SongFormPost.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { postSongRequest } from '../../store/songs'
 import { fetchSingleAlbum } from '../../store/albums'
@@ -16,7 +16,7 @@ export default function SongFormPost({ albumId }) {
     const [ errors, setErrors ] = useState({})
     const { closeModal } = useModal()
     const [ hasSubmitted, setHasSubmitted ] = useState(false)
-    const validate = () => {
+    useEffect(() => {
         const err = {}
         if (!name) err.name = 'Please enter a title for your song'
         if (!price || price < 0 || price > 100) err.price = 'Please enter a realistic price for your song'
@@ -24,13 +24,11 @@ export default function SongFormPost({ albumId }) {
         if (!url) err.url = 'Please provide a path to upload your song'
         setErrors(err)
         return err
-    }
+    }, [name, price, trackNum, url])
 
     const handleSubmit = e => {
         e.preventDefault();
-        validate()
         setHasSubmitted(true)
-
         if (Object.values(errors).length) return alert('Please correct input errors')
         else {
             const newSong = { name, lyrics, price: parseFloat(price), track_num: parseInt(trackNum), url, album_id: parseInt(albumId) }
@@ -45,13 +43,13 @@ export default function SongFormPost({ albumId }) {
         <div className='post-song-form-page'>
         <h3 className='post-song-title'>add a song to album</h3>
 
-        <form className='post-song-form'>
+        <form className='post-song-form' onSubmit={handleSubmit}>
         <div className='post-song-form-grid'>
         <label className='post-song-label'>name</label>
 
         <div className='post-song-col'>
         <input type='text' className='post-song-input' value={name} onChange={e => setName(e.target.value)}></input>
-        {hasSubmitted && !Object.values(errors).length ? (
+        {hasSubmitted && errors.name ? (
         <p className='errors'>{errors.name}</p>
         ) : (
         <p></p>
@@ -61,8 +59,8 @@ export default function SongFormPost({ albumId }) {
 
         <div className='post-song-col'>
         <textarea type='textarea' className='post-song-input post-song-lyrics' value={lyrics} onChange={e => setLyrics(e.target.value)}></textarea>
-        {hasSubmitted && !Object.values(errors).length ? (
-        <p className='errors'>{errors.name}</p>
+        {hasSubmitted && errors.lyrics ? (
+        <p className='errors'>{errors.lyrics}</p>
         ) : (
         <p></p>
         )}
@@ -72,8 +70,8 @@ export default function SongFormPost({ albumId }) {
 
         <div className='post-song-col'>
         <input type='number' className='post-song-input' value={price} onChange={e=> setPrice(e.target.value)}></input>
-        {hasSubmitted && !Object.values(errors).length ? (
-        <p className='errors'>{errors.name}</p>
+        {hasSubmitted && errors.price ? (
+        <p className='errors'>{errors.price}</p>
         ) : (
         <p></p>
         )}
@@ -83,8 +81,8 @@ export default function SongFormPost({ albumId }) {
 
         <div className='post-song-col'>
         <input type='number' className='post-song-input' value={trackNum} onChange={e=> setTrackNum(e.target.value)} min={0} max={100}></input>
-        {hasSubmitted && !Object.values(errors).length ? (
-        <p className='errors'>{errors.name}</p>
+        {hasSubmitted && errors.trackNum ? (
+        <p className='errors'>{errors.trackNum}</p>
         ) : (
         <p></p>
         )}
@@ -94,8 +92,8 @@ export default function SongFormPost({ albumId }) {
 
         <div className='post-song-col'>
         <input type='url' className='post-song-input' value={url} onChange={e=> setUrl(e.target.value)}></input>
-        {hasSubmitted && !Object.values(errors).length ? (
-        <p className='errors'>{errors.name}</p>
+        {hasSubmitted && errors.url ? (
+        <p className='errors'>{errors.url}</p>
         ) : (
         <p></p>
         )}
