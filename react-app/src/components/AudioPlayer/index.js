f8a5df40ec1d1d2fb2afe5f986deb57e34d9839d
cludwell <cludwell@gmail.com>
2023-06-27 23:19:09 -0700

    css and audio component

--- react-app/src/components/AudioPlayer/index.js
+++ react-app/src/components/AudioPlayer/index.js
@@ -3,68 +3,75 @@ import './AudioPlayer.css';
 import { useState, useRef, useEffect } from 'react';
 
 export default function AudioPlayer({ song }) {
-  const [ play, setPlay ] = useState(false);
-  const [ time, setTime ] = useState('0:00')
-  const playerRef = useRef(null);
-  const [ duration, setDuration ] = useState()
+    //state
+    const [ isPlaying, setIsPlaying ] = useState(false);
+    const [ time, setTime ] = useState(0)
+    const [ duration, setDuration ] = useState(0)
 
-  useEffect(() => {
-      const currentTime = () => {
-        const media = playerRef.current.getCurrentTime()
-        const minutes = Math.floor(media/60)
-        const seconds = parseInt(media % 60)
-        if (playerRef.current && playerRef.current.getCurrentTime() > 0) setTime(`${minutes}:${seconds}`)
-      }
-      const currentDuration = () => {
-        const media = playerRef.current.getDuration()
-        const minutes = Math.floor(media/60)
-        const seconds = parseInt(media % 60)
-        setDuration(`${minutes}:${seconds}`)
-      }
-      currentTime()
-      currentDuration()
-  }, [time])
+    //references
+    const audioPlayer = useRef();
+    const progressBar = useRef();
+    const animationRef = useRef();
 
-  const togglePlayPause = () => {
-    setPlay(!play);
-  };
-console.log('====================================', song)
+    useEffect(() => {
+        const seconds = Math.floor(audioPlayer.current.duration);
+        setDuration(seconds);
+        progressBar.current.max = seconds;
+    }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState])
+
+    const calculateTime = (secs) => {
+        const minutes = Math.floor(secs/60)
+        const seconds = parseInt(secs % 60)
+        return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+    }
+    
+    const togglePlayPause = () => {
+        const prevValue = isPlaying
+        setIsPlaying(!prevValue); // Use functional form of setState
+        if (!prevValue) {
+            audioPlayer.current.play()
+            animationRef.current = requestAnimationFrame(whilePlaying)
+        } else {
+            audioPlayer.current.pause()
+            cancelAnimationFrame(animationRef.current)
+        }
+    };
+
+    const changePlayerTime = () => {
+        progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`)
+        setTime(progressBar.current.value)
+    }
+    const whilePlaying = () => {
+        progressBar.current.value = audioPlayer.current.currentTime;
+        changePlayerTime()
+        animationRef.current = requestAnimationFrame(whilePlaying)
+    }
+    const changeRange = () => {
+        audioPlayer.current.currentTime = progressBar.current.value;
+        changePlayerTime();
+    }
+
   return (
     <div className='custom-player'>
       <button className='play-button' onClick={togglePlayPause}>
-        {play ? <i className='fa-solid fa-pause' />
+        {isPlaying ? <i className='fa-solid fa-pause' />
         : <i className='fa-solid fa-play' />}
       </button>
 
-      <div>
-        {song.trackNum}: {song.name}
-      </div>
-      <div>
-        {time} / {duration}
-      </div>
-      <input
-      type='range'
-      value={time}
-
-      onChange={e => setTime(e.target.value)}
-      ></input>
-
-      <ReactPlayer
-        ref={playerRef} // Assigning the ref
-        url={song.url}
-        controls={true}
-        className='player'
-        playsinline={true}
-        config={{
-          file: {
-            attributes: {
-              controlsList: 'nodownload',
-            },
-          },
-        }}
-        width={'37vmin'}
-        height={'6vmin'}
-      />
+    <div>
+        <div>{song.trackNum}: {song.name}</div>
+        <input
+        type='range'
+        ref={progressBar}
+        defaultValue={0}
+        value={time}
+        onChange={changeRange}
+        ></input>
+        <span>
+            {calculateTime(time)} / {calculateTime(duration)}
+        </span>
+        <audio src={song.url} ref={audioPlayer}> </audio>
+    </div>
     </div>
   );
 }
