1b527f272d1912a4b5b18a603ee271e2174ef520
cludwell <cludwell@gmail.com>
2023-06-28 10:42:16 -0700

    aws errors

--- app/api/aws_helpers.py
+++ app/api/aws_helpers.py
@@ -10,7 +10,7 @@ ALLOWED_SONGS = {"mp3", "wav", "ogg", "aac", "m4a"}
 
 s3 = boto3.client(
    "s3",
-   aws_access_key_id=os.environ["S3_KEY"],
+   aws_access_key_id=os.environ.get("S3_KEY"),
    aws_secret_access_key=os.environ.get("S3_SECRET")
 )
 
