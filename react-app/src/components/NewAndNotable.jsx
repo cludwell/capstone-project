3bf68570c2409ec4476f01c526479d6a944067d0
cludwell <cludwell@gmail.com>
2024-02-15 20:35:18 -0800

    truncate and line clamp

--- react-app/src/components/NewAndNotable.jsx
+++ react-app/src/components/NewAndNotable.jsx
@@ -10,13 +10,13 @@ export default function NewAndNotable({ album }) {
           alt="album-im"
           className=" aspect-square object-cover rounded-t-lg w-56"
         />
-        <div className=" text-sm p-2 w-56 h-80 overflow-hidden">
-          <section className=" italic text-base ">
+        <div className=" text-sm p-2 w-56 h-64 overflow-hidden">
+          <section className=" italic text-base truncate">
             {album.name.toUpperCase()}
           </section>
           <section className=" text-purple-800 text-xs">{album.genre}</section>
-          <section className=" text-xs ">
-            {album.description.slice(0,500)}
+          <section className=" text-xs line-clamp">
+            {album.description}
           </section>
         </div>
       </div>
